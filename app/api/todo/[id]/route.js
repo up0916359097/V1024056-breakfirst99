c3de2973861528ef38781eb5eda160d3e1eb4dc6
向上0916359097 <up0916359097@gmail.com>
2025-04-30 17:55:11 +0800

    successful

---
+++ app/api/todo/[id]/route.js
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request, {params}){
+    const {id} =await params;
+    const todo =await prisma.todo.findUnique({
+        where: {id: Number(id)},
+    });
+
+    if(!todo) {
+        return NextResponse.json(new Error(`Todo with id $ {id} not found`),{
+            status:404,
+        });
+    }
+    return NextResponse.json(todo);
+}
+
+export async function DELETE(request, {params}){
+    const {id} = await params;
+    try{
+        const todo = await prisma.todo.delete({
+            where:{ id:Number(id)},
+        });
+        return NextResponse.json(todo);
+    }catch(error){
+        return NextResponse.json(new errorMap(`Todo with id $ {id} not found`),{
+            status:404,
+        });
+    }
+}
+
+export async function PATCH(request,{ params }){
+    const {id} =await params;
+    const body = await request.json();
+    try{
+        const todo =await prisma.todo.update({
+            where:{ id:Number(id)},
+            data:body,
+        });
+        return NextResponse.json(todo);
+    }catch(error){
+        return NextResponse.json(new Error(`Todo with id $ {id} not found`),{
+            status: 404,
+        });
+    }
+}
--- app/api/todo/route.js
+++ app/api/todo/route.js
@@ -11,10 +11,30 @@ export async function GET(request){
     if (!queryStr){
         return NextResponse.json(todos);
     }    
-
+    console.log(1);
+    
     queryStr = queryStr.toLowerCase();
     todos = todos.filter((todo) => todo.title.includes(queryStr));
-    if (!todos) {
-        return NextResponse.error(new Error(`No todos with ${queryStr} exists`),{status: 404});
+    console.log(2);
+    if (todos.length===0) {
+        return NextResponse.json(
+            {error:`No todos with ${queryStr} exists`},
+            {status: 404}
+        );
+    }
+    console.log(3);
+}
+
+export async function POST(request){
+    const {title} =await request.json();
+    try{
+        const todo =await prisma.todo.create({
+            data:{title},
+        });
+        return NextResponse.json(todo);
+    }catch(error){
+        return NextResponse.json(new Error("Error creating todo"),{
+            status: 500,
+        });
     }
 }
